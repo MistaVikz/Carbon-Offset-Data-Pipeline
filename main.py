7a7f8d8c3418d74515cfc9b3140661a387e458dc
Mark Vickers <markdvickers@icloud.com>
2026-08-14 15:29:38 -0600

    fix: Included verra_project file with old (supported format)

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ def main():
     include_cdm_only = args.cdm_only
     master_input_file = args.create_master_gold
 
-    # Turn verra off (API data has changed)
+    # Turn verra updates off (API data has changed)
     update_verra = False
-    include_verra = False
+    
 
     # Create output folder and reset warning log
     if include_cdm_only:
